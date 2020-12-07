8b66c743ec1209091d911a2f40d403d9561ccbdf
Rodrigo Passos <rodrigo@hellodative.com>
2020-12-07 11:19:37 -0500

    clean up indentation and initial commit

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 
-    // Create Dino Constructor
+// Create Dino Constructor
 
 
-    // Create Dino Objects
+// Create Dino Objects
 
 
-    // Create Human Object
+// Create Human Object
 
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Use IIFE to get human data from form
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Remove form from screen
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+    // Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
