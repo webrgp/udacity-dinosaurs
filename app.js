f23d92cd008fc6bc3a031830ec26764775f48d33
Rodrigo Passos <rodrigo@hellodative.com>
2020-12-07 13:57:31 -0500

    add dino loading and basic form handleing

--- app.js
+++ app.js
@@ -1,23 +1,103 @@
-(function() {
+let app = (function() {
 
-  // Create Dino Constructor
-  function Dino(species, weight, height, diet, where, when, fact) {
-    this.species = species
-    this.weight  = weight
-    this.height  = height
-    this.diet    = diet
-    this.where   = where
-    this.when    = when
-    this.fact    = fact
+  // UI
+  const form = document.getElementById('dino-compare');
+
+  let dinos = []
+
+  const basicAnimal = {
+    weight: 0,
+    height: 0,
+    diet: ''
   }
 
-}());
+  // Create an object representing a dinosaur.
+  function _dinoFactory(species, weight, height, diet, where, when, fact) {
+    return Object.assign({}, basicAnimal, {
+			species: species,
+			weight: weight,
+			height: height,
+			diet: diet,
+			where: where,
+			when: when,
+      fact: fact,
+      compareHeight: function(human) {
+        const diff = this.height - human.height
+        if (diff === 0) {
+          return `You are the same height as ${this.species}`
+        }
 
+        if (diff > 0) {
+          return `You are shorter than ${this.species}`
+        }
 
+        if (diff < 0) {
+          return `You are taller than ${this.species}`
+        }
+      }
+		})
+  }
 
+  // Create an object representing a human.
+  function _humanFactory (name, height, weight, diet) {
+    return Object.assign({}, basicAnimal, {
+      name: name,
+      height: height,
+      weight: weight,
+      diet: diet
+    })
+  }
 
-// Create Dino Objects
+  // Create Dino Objects
+  async function _loadDinos() {
+    const data = await _loadDinoData()
+    if ('Dinos' in data) {
+      data.Dinos.forEach( function(dino) {
+        dinos.push(_dinoFactory(
+          dino.species,
+          dino.weight,
+          dino.height,
+          dino.diet,
+          dino.where,
+          dino.when,
+          dino.fact
+        ))
+      })
+    }
+  }
 
+  async function _loadDinoData() {
+    const url = './dino.json'
+    try {
+      const response = await fetch(url, {
+        mode: 'no-cors'
+      })
+      return response.json()
+    } catch (e) {
+      console.log('Error loading dino data:', e);
+    }
+  }
+
+  function _initEventListeners() {
+    form.addEventListener('submit', function(event){
+      console.log(`Form Submitted! Time stamp: ${event.timeStamp}`);
+      event.preventDefault();
+    })
+  }
+
+  async function init() {
+    console.log('Initialize application')
+    await _loadDinos()
+    _initEventListeners()
+  }
+
+  return {
+    init: init
+  }
+
+}())
+
+app.init()
 
 
 // Create Human Object
